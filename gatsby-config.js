fac6db605466652c616dcd74e46e4ce53cd22537
Chris Shimmin <shimmin.christopher@gmail.com>
2021-08-07 12:12:48 -0400

    page updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Dynasty League`,
+    description: `Fantasy Football Dynasty League`,
+    author: `Chris Shimmin`,
+    siteUrl: `/`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
